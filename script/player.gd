4511d40e376fb07b70f0f9cff0ac058d5f1ff63b
m9k3s <126334796+m9k3s@users.noreply.github.com>
2025-07-10 21:46:33 +0800

    player code for movement wasd

--- script/player.gd
+++ script/player.gd
@@ -1,11 +1,27 @@
-extends CharacterBody2D
+extends CharacterBody2D #node type important
+
+var speed = 200 #set the speed
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# create a new var to hold the object direction
+	var direction = Vector2()
+	
+	if Input.is_action_pressed("w"): # check the key press using the input map
+		direction.y -= 1 #minus the vector to change the direction of object 
+	if Input.is_action_pressed("a"):
+		direction.x -= 1
+	if Input.is_action_pressed("s"):
+		direction.y += 1
+	if Input.is_action_pressed("d"):
+		direction.x += 1
+	
+	direction = direction.normalized() # to make speed consistent
+	velocity = direction * speed # to set the velocity to direction and speed
+	
+	move_and_slide() #to move the object
